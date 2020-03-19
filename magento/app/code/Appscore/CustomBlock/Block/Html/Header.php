f6e1e7f045093b17f9327d76d70abcd154aec67c
bao-appscore <32353681+bao-appscore@users.noreply.github.com>
2020-03-19 16:58:58 +1100

    finish header and menu backend

---
+++ magento/app/code/Appscore/CustomBlock/Block/Html/Header.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Appscore\CustomBlock\Block\Html;
+
+/**
+ * Html page header block
+ *
+ * @api
+ * @since 100.0.2
+ */
+class Header extends \Magento\Theme\Block\Html\Header
+{
+    /**
+     * @var \Magento\Customer\Model\Url
+     */
+    protected $_customerUrl;
+
+    /**
+     * @var \Magento\Wishlist\Helper\Data
+     */
+    protected $_wishlistHelper;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Model\Url $customerUrl,
+        \Magento\Wishlist\Helper\Data $wishlistHelper,
+        array $data = []
+    ) {
+        $this->_customerUrl = $customerUrl;
+        $this->_wishlistHelper = $wishlistHelper;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Retrieve welcome text
+     *
+     * @return string
+     */
+    public function getWelcome()
+    {
+        if (empty($this->_data['welcome'])) {
+            $this->_data['welcome'] = $this->_scopeConfig->getValue(
+                'design/header/welcome',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+        }
+        return __($this->_data['welcome']);
+    }
+
+    public function getCustomerUrl()
+    {
+        return $this->_customerUrl->getAccountUrl();
+    }
+
+    public function getWishlistUrl()
+    {
+        if ($this->_wishlistHelper->isAllow()) {
+            return $this->getUrl('wishlist');
+        }
+    }
+}
--- magento/app/etc/config.php
+++ magento/app/etc/config.php
@@ -233,6 +233,7 @@ return [
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
+        'Appscore_CustomBlock' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'Klarna_Core' => 1,
