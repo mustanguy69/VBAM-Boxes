1488ef087fb252459adeaabbb2d6f19ced5f6dbc
bao-appscore <32353681+bao-appscore@users.noreply.github.com>
2020-03-20 17:39:50 +1100

    finish with the skeleton for footer and header

---
+++ magento/app/design/frontend/Appscore/VBAM/web/js/custom.js
@@ -0,0 +1,64 @@
+define([
+        'jquery'
+    ], function($){
+
+        "use strict";
+
+        $(document).ready(function($){
+            var $window = $(window);
+
+            function checkWidthForNavigation() {
+                var windowsize = $window.width();
+                if (windowsize > 768) {
+                    parentHover();
+                } else {
+                    parentClick();
+                }
+            }
+
+            function parentHover() {
+                $(".navigation__item--parent").prop("onclick", null).off("click");
+                $(".navigation__item--parent").hover(
+                    function() {
+                        $(this).children('.submenu').stop(true, true).fadeIn('medium');
+                    },
+                    function() {
+                        $(this).children('.submenu').stop(true, true).fadeOut('medium');
+                    }
+                );
+            }
+
+            function parentClick() {
+                $(".navigation__item--parent").unbind('mouseenter mouseleave');
+                $(".navigation__item--parent").click(
+                    function() {
+                        $(this).children('.submenu').stop(true, true).slideToggle('medium');
+                    }
+                );
+            }
+
+            function clickToCloseMobileMenu() {
+                $('.close-menu').click(
+                    function() {
+                        $('html').removeClass('nav-open');
+
+                        //timeout
+                        setTimeout(function() {
+                            $('html').removeClass('nav-before-open');
+                        }, 200)
+                    }
+                )
+            }
+
+            // checking for window resize
+            $(window).resize(function() {
+                checkWidthForNavigation();
+            });
+
+            // initial load
+            checkWidthForNavigation();
+            clickToCloseMobileMenu();
+
+        });
+        return;
+});
