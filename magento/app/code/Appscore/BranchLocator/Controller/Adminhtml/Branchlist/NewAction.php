249fc33e8f3c1ce9def324d098f11666879a5d5d
tanguy-appscore <62579286+tanguy-appscore@users.noreply.github.com>
2020-04-07 11:02:31 +1000

    Remove comment

--- magento/app/code/Appscore/BranchLocator/Controller/Adminhtml/Branchlist/NewAction.php
+++ magento/app/code/Appscore/BranchLocator/Controller/Adminhtml/Branchlist/NewAction.php
@@ -6,11 +6,6 @@ use Appscore\Branchlocator\Controller\Adminhtml\Branchlist;
 
 class NewAction extends Branchlist
 {
-   /**
-     * Create new news action
-     *
-     * @return void
-     */
    public function execute()
    {
       $this->_forward('edit');
