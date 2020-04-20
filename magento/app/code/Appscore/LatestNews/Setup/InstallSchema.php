f159ca7ab1f198687eeb0e4384b6dab20f65056a
bao-appscore <32353681+bao-appscore@users.noreply.github.com>
2020-04-20 17:28:22 +1000

    fixed some styling issue

--- magento/app/code/Appscore/LatestNews/Setup/InstallSchema.php
+++ magento/app/code/Appscore/LatestNews/Setup/InstallSchema.php
@@ -33,131 +33,135 @@ class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
 
 	public function install(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
 	{
-		$installer = $setup;
-		$installer->startSetup();
-		if (!$installer->tableExists('appscore_latestnews_news')) {
-			$table = $installer->getConnection()->newTable(
-				$installer->getTable('appscore_latestnews_news')
-			)
-				->addColumn(
-					'id',
-					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-					null,
-					[
-						'identity' => true,
-						'nullable' => false,
-						'primary'  => true,
-						'unsigned' => true,
-					],
-					'News ID'
+		try {
+			$installer = $setup;
+			$installer->startSetup();
+			if (!$installer->tableExists('appscore_latestnews_news')) {
+				$table = $installer->getConnection()->newTable(
+					$installer->getTable('appscore_latestnews_news')
 				)
-				->addColumn(
-					'title',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					255,
-					['nullable => false'],
-					'News Title'
+					->addColumn(
+						'id',
+						\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+						null,
+						[
+							'identity' => true,
+							'nullable' => false,
+							'primary'  => true,
+							'unsigned' => true,
+						],
+						'News ID'
+					)
+					->addColumn(
+						'title',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+						255,
+						['nullable => false'],
+						'News Title'
+					)
+					->addColumn(
+						'url_key',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+						255,
+						[],
+						'News URL Key'
+					)
+					->addColumn(
+						'short_content',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+						'64k',
+						[],
+						'News Short Content'
+					)
+					->addColumn(
+						'content',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+						'64k',
+						[],
+						'News Content'
+					)
+					->addColumn(
+						'status',
+						\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+						1,
+						[],
+						'News Status'
+					)
+					->addColumn(
+						'image',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+						255,
+						[],
+						'News Image'
+					)
+					->addColumn(
+						'category_id',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+						255,
+						[],
+						'News Category'
+					)
+					->addColumn(
+						'created_at',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+						null,
+						['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+						'Created At'
+					)->addColumn(
+						'updated_at',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+						null,
+						['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+						'Updated At');
+				$installer->getConnection()->createTable($table);
+			}
+
+			if (!$installer->tableExists('appscore_latestnews_categories')) {
+				$table = $installer->getConnection()->newTable(
+					$installer->getTable('appscore_latestnews_categories')
 				)
-				->addColumn(
-					'url_key',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					255,
-					[],
-					'News URL Key'
-				)
-				->addColumn(
-					'short_content',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					'64k',
-					[],
-					'News Short Content'
-				)
-				->addColumn(
-					'content',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					'64k',
-					[],
-					'News Content'
-				)
-				->addColumn(
-					'status',
-					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-					1,
-					[],
-					'News Status'
-				)
-				->addColumn(
-					'image',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					255,
-					[],
-					'News Image'
-				)
-				->addColumn(
-					'category_id',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					255,
-					[],
-					'News Category'
-				)
-				->addColumn(
-					'created_at',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-					null,
-					['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
-					'Created At'
-				)->addColumn(
-					'updated_at',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-					null,
-					['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
-					'Updated At');
-			$installer->getConnection()->createTable($table);
+					->addColumn(
+						'id',
+						\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+						null,
+						[
+							'identity' => true,
+							'nullable' => false,
+							'primary'  => true,
+							'unsigned' => true,
+						],
+						'Category ID'
+					)
+					->addColumn(
+						'name',
+						\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+						255,
+						['nullable => false'],
+						'Category Name'
+					);
+				$installer->getConnection()->createTable($table);
+			}
+			$installer->endSetup();
+
+			$this->state->setAreaCode(Area::AREA_ADMINHTML);
+		
+			/** @var BlockInterface $cmsBlock */
+			$cmsBlock = $this->blockInterfaceFactory->create();
+
+			$content = <<<HTML
+			<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mauris tortor, 
+			rhoncus at diam ac, aliquet egestas elit. Integer varius, ipsum et imperdiet scelerisque, 
+			lorem magna feugiat arcu, sed rhoncus velit magna a velit.</div> 
+			HTML;
+
+			$cmsBlock->setIdentifier('about_us');
+			$cmsBlock->setTitle('About Us');
+			$cmsBlock->setContent($content);
+			$cmsBlock->setData('stores', [Store::DEFAULT_STORE_ID]);
+
+			$this->blockRepository->save($cmsBlock);
+		} catch (\Exception $e) {
+			$this->logger->critical($e->getMessage());
 		}
-
-		if (!$installer->tableExists('appscore_latestnews_categories')) {
-			$table = $installer->getConnection()->newTable(
-				$installer->getTable('appscore_latestnews_categories')
-			)
-				->addColumn(
-					'id',
-					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-					null,
-					[
-						'identity' => true,
-						'nullable' => false,
-						'primary'  => true,
-						'unsigned' => true,
-					],
-					'Category ID'
-				)
-				->addColumn(
-					'name',
-					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					255,
-					['nullable => false'],
-					'Category Name'
-				);
-			$installer->getConnection()->createTable($table);
-		}
-		$installer->endSetup();
-
-		$this->state->setAreaCode(Area::AREA_ADMINHTML);
-      
-		/** @var BlockInterface $cmsBlock */
-		$cmsBlock = $this->blockInterfaceFactory->create();
-
-		$content = <<<HTML
-		<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mauris tortor, 
-		rhoncus at diam ac, aliquet egestas elit. Integer varius, ipsum et imperdiet scelerisque, 
-		lorem magna feugiat arcu, sed rhoncus velit magna a velit.</div> 
-		HTML;
-
-		$cmsBlock->setIdentifier('about_us');
-		$cmsBlock->setTitle('About Us');
-		$cmsBlock->setContent($content);
-		$cmsBlock->setData('stores', [Store::DEFAULT_STORE_ID]);
-
-		$this->blockRepository->save($cmsBlock);
 	}
 }
